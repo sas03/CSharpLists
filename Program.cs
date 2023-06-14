170d95d084c1bb76527efd91e8f1fc51b4a19fad
sas03 <stephanesumo@yahoo.fr>
2023-06-14 14:56:11 +0200

    Lists

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Lists;
+class Program
+{
+    public static void Display(List<string> liste){
+        foreach(string s in liste){
+            Console.WriteLine(s);
+        }
+    }
+    static void Main(string[] args)
+    {
+        List<string> exemple = new List<string>();
+        exemple.Add("Premier");
+        exemple.Add("Deuxième");
+        exemple.Add("Troisième");
+
+        Console.WriteLine("First Round: ");
+        Display(exemple);
+
+        Console.WriteLine("Second Round: ");
+        exemple.RemoveAt(0);
+        Display(exemple);
+
+        Console.WriteLine("Third Round: ");
+        exemple.Remove("Troisième");
+        Display(exemple);
+
+        Console.WriteLine("Final Round: ");
+        exemple[0] = "Modif";
+        Display(exemple);
+    }
+}
